1e9134b9eb55b78278d3786c63f4204b55575b2a
John Mac <mcdona22@gmail.com>
2017-07-16 12:09:32 +0100

    refactoring some services

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import {PersonService} from './service/person.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'] ,
+  providers: [PersonService]
 })
 export class AppComponent {
   title = 'Connectr';
